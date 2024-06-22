b47de624806ea29db3afa1c6a955e73b6e2ffe48
sah alom <sahalom4729@gmail.com>
2024-06-22 16:57:53 +0400

    added detailse page

--- src/app/services/[id]/page.jsx
+++ src/app/services/[id]/page.jsx
@@ -1,21 +1,42 @@
 import { getServiceDetails } from '@/services/getServices';
+import Image from 'next/image';
 import React from 'react';
 
-const getDetails = async()=>{
+const getDetails = async () => {
 
-   
+
 }
 
-const ServiceDetails = async({params}) => {
-const id = params.id
-const data = await getServiceDetails(id)
-console.log(data);
+const ServiceDetails = async ({ params }) => {
+    const id = params.id
+    const data = await getServiceDetails(id)
+    console.log(data);
     return (
-        <div>
+        <div className='max-w-7xl m-auto'>
             <div>
-                <h1 className='text-3xl font-bold my-5'>Details </h1>
+                <h1 className='text-3xl font-bold m-5'>Details </h1>
             </div>
-           <h1>{data?.title}</h1>
+            <div className='p-8  my-6'>
+                <Image src={data.img} alt='img' width={700} height={500}></Image>
+                <div className='bg-neutral-400 p-3'>
+
+                    <h1 className='text-xl text-p-color font-bold'>{data?.title}</h1>
+                    <p>{data.description}</p>
+                </div>
+
+                <div>
+                    <div className=' grid grid-cols-2 gap-3'>
+                        {
+                            data.facility.map((f,i)=><div key={i} className=' p-2 border-t-2  border-p-color  rounded-lg '>
+                                <h1 className='font-bold'>{f.name}</h1>
+                                <p>{f.description}</p>
+
+                            </div>)
+                        }
+                    </div>
+                </div>
+            </div>
+            <h1 className='text-xl text-p-color'>Price:{data?.price}</h1>
         </div>
     );
 };
